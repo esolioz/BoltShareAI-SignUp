251aedde6322546159fdcc199f22db42c8d1fe35
Eric Solioz <dev@eric-solioz.com>
2023-05-05 17:14:57 +0200

    Add stepper component and onboarding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OnboardingComponent } from './onboarding/onboarding.component';
+import { StepperComponent } from './shared/stepper/stepper/stepper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnboardingComponent,
+    StepperComponent
   ],
   imports: [
     BrowserModule,
