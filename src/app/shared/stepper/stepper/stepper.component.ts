1262cf4d18232a500f4c7ecfb39dfeca00b26a59
Eric Solioz <dev@eric-solioz.com>
2023-05-06 13:43:01 +0200

    Implement the stepper cdk

--- src/app/shared/stepper/stepper/stepper.component.ts
+++ src/app/shared/stepper/stepper/stepper.component.ts
@@ -1,10 +1,10 @@
+import { CdkStepper } from '@angular/cdk/stepper';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
-  styleUrls: ['./stepper.component.scss']
+  styleUrls: ['./stepper.component.scss'],
+  providers: [{ provide: CdkStepper, useExisting: StepperComponent }],
 })
-export class StepperComponent {
-
-}
+export class StepperComponent extends CdkStepper {}
