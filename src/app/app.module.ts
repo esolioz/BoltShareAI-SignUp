7a965a2f453c2b76c764df128d1b8c551f4189c7
Eric Solioz <dev@eric-solioz.com>
2023-05-06 12:48:19 +0200

    Create the  basic inputs fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,29 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OnboardingComponent } from './onboarding/onboarding.component';
 import { StepperComponent } from './shared/stepper/stepper/stepper.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
+import { CdkStepperModule } from '@angular/cdk/stepper';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    OnboardingComponent,
-    StepperComponent
-  ],
+  declarations: [AppComponent, OnboardingComponent, StepperComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+
+    // Material modules
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/onboarding/onboarding.component.ts
+++ src/app/onboarding/onboarding.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-onboarding',
   templateUrl: './onboarding.component.html',
-  styleUrls: ['./onboarding.component.scss']
+  styleUrls: ['./onboarding.component.scss'],
 })
 export class OnboardingComponent {
+  /**
+   * User details
+   */
+  formUserDetails = this._formBuilder.group({
+    firstName: [''],
+    lastName: [''],
+    username: [''],
+    email: [''],
+  });
 
+  /**
+   * Password
+   */
+  formPassword = this._formBuilder.group({
+    password: [''],
+    confirmPassword: [''],
+  });
+
+  /**
+   * Payment
+   */
+  formPayment = this._formBuilder.group({
+    creditCardNumber: [''],
+    creditCardExpiryDate: [''],
+    creditCardCvvCode: [''],
+    street: [''],
+    postalCode: [''],
+    city: [''],
+    country: [''],
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
 }
